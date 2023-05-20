339c99897e0d323083dbd84550a3959ae10ca806
hiromikoreyes <114784445+hiromikoreyes@users.noreply.github.com>
2023-05-20 00:11:05 -0400

    Added json dump functionality

--- scraper.py
+++ scraper.py
@@ -46,13 +46,50 @@ def blacklist_filter(links, filter_list):
         if(addFlag == True): new.append(link)
     return new
 
-# def get_body_content(html):
-#     """Only works for wikipedia"""
-#     html.p
+def get_title(html):
+    return html.find(class_="mw-page-title-main").get_text()   
+
+def get_body_content(html, maxlen):
+    """Only works for wikipedia"""
+    body = ""
+    body_list = []
+    tags = html.find_all("p")
+
+    for item in tags:
+        body += item.get_text()
+        if len(body) >= maxlen: return body
+    return body
+
+def make_json(html):
+    data = {"title": get_title(html), "content": get_body_content(html, 1000).strip()}
+    json_object = json.dumps(data, indent=4)
+    with open("sample.json", 'w') as outfile:
+        outfile.write(json_object)
+
+def update_json(filepath, html):
+    data = []
+    with open(filepath, 'r') as outfile:
+        try: data = json.load(outfile)
+        except: data = []
+        data.append({"title": get_title(html), "content": get_body_content(html, 1000).strip()})
+    with open(filepath, 'w') as outfile:
+        outfile.write(json.dumps(data, indent=4))
+
+            
+
+
+    
 
 mess_list = get_links(main_html)
 link_list = whitelist_filter(mess_list, '/wiki/')
 link_list = blacklist_filter(link_list, ['File:', 'Special:', 'Talk:', 'Category:', 'Help:', '(identifier)', 'Wikipedia:', '(disambiguation)'])
+# make_json(main_html)
+update_json("sample.json", main_html)
+
+# f = open('sample.json',)
+# data = json.load(f)
+
+# print(data)
 
 # for x in link_list:
 #     print(x)
